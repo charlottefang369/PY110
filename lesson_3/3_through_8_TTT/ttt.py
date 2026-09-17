1a682acc337d3a8b2a6324df587a30bba974a2dc
Charlotte Fang <charlottefang369@gmail.com>
2026-09-17 20:01:27 +0800

    ttt file created

---
+++ lesson_3/3_through_8_TTT/ttt.py
@@ -0,0 +1,21 @@
+
+
+def display_board(board): 
+    print('     |     |')
+    print(f'  {board[1]}  |  {board[2]}  |  {board[3]}')
+    print('     |     |')
+    print('-----+-----+-----')
+    print('     |     |')
+    print(f'  {board[4]}  |  {board[5]}  |  {board[6]}')
+    print('     |     |')
+    print('-----+-----+-----')
+    print('     |     |')
+    print(f'  {board[7]}  |  {board[8]}  |  {board[9]}')
+    print('     |     |')  
+
+def initialize_board(): 
+    return {square: ' ' for square in range(1, 10)}
+
+board = initialize_board()
+
+display_board(board)
