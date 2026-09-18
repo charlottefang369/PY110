ae60ecac1f81f880c5ecee7fe6e56686dca73488
Charlotte Fang <charlottefang369@gmail.com>
2026-09-18 19:30:16 +0800

    working on TTT

--- lesson_3/3_through_8_TTT/ttt.py
+++ lesson_3/3_through_8_TTT/ttt.py
@@ -1,21 +1,82 @@
+import os
+import random
 
+INITIAL_MARKER = ' '
+HUMAN_MARKER = 'X'
+COMPUTER_MARKER = 'O'
 
-def display_board(board): 
+def prompt(message):
+    print(f"=> {message}")
+
+def display_board(board):
+    os.system('clear')
+    print('')
     print('     |     |')
-    print(f'  {board[1]}  |  {board[2]}  |  {board[3]}')
+    print(f"  {board[1]}  |  {board[2]}  |  {board[3]}")
     print('     |     |')
     print('-----+-----+-----')
     print('     |     |')
-    print(f'  {board[4]}  |  {board[5]}  |  {board[6]}')
+    print(f"  {board[4]}  |  {board[5]}  |  {board[6]}")
     print('     |     |')
     print('-----+-----+-----')
     print('     |     |')
-    print(f'  {board[7]}  |  {board[8]}  |  {board[9]}')
-    print('     |     |')  
+    print(f"  {board[7]}  |  {board[8]}  |  {board[9]}")
+    print('     |     |')
+    print('')
 
-def initialize_board(): 
-    return {square: ' ' for square in range(1, 10)}
+def initialize_board():
+    return {square: INITIAL_MARKER for square in range(1, 10)}
+
+def empty_squares(board):
+    return [key for key, value in board.items() if value == INITIAL_MARKER]
+
+def player_chooses_square(board):
+    while True:
+        valid_choices = [str(num) for num in empty_squares(board)]
+        prompt(f"Choose a square ({', '.join(valid_choices)}):")
+        square = input().strip()
+        if square in valid_choices:
+            break
+
+        prompt("Sorry, that's not a valid choice.")
+
+    board[int(square)] = HUMAN_MARKER
+
+def computer_chooses_square(board):
+    if len(empty_squares(board)) == 0:
+        return
+    square = random.choice(empty_squares(board))
+    board[square] = COMPUTER_MARKER
+
+def board_full(board):
+    return len(empty_squares(board)) == 0
+
+def someone_won(board):
+    return False
 
 board = initialize_board()
-
 display_board(board)
+
+while True:
+    player_chooses_square(board)
+    computer_chooses_square(board)
+    display_board(board)
+
+    if someone_won(board) or board_full(board):
+        break
+
+
+
+
+
+# board = {
+#   1: ' ', # top left
+#   2: ' ', # top center
+#   3: ' ', # top right
+#   4: ' ', # middle left
+#   5: ' ', # center
+#   6: ' ', # middle right
+#   7: ' ', # bottom left
+#   8: ' ', # bottom center
+#   9: ' ', # bottom right
+# }
